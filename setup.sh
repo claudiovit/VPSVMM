4bb953f1dbaa9e5fe48672224c3c630a3a8b2b5b
claudiovit <klaudio01@gmail.com>
2017-01-31 10:29:07 -0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,15 +3,18 @@ clear
 echo -e "\033[42;1;37m                              SCRIPT  DE INSTALAÇAO DO                                \033[0m"
 echo -e "\033[42;1;37m                                VPS MANAGER VITAL MAX                                 \033[0m"
 echo
-echo -e "\033[0;31mQual a sua senha de acesso (Do \033[00;37m""\033[01;34mVITAL VMM\033[00;37m""\033[0;31m)?\033[00;37m"
+echo -e "\033[01;32mQual a sua senha de acesso (Do \033[00;37m""\033[01;34mVMM MAX\033[00;37m""\033[01;32m)?\033[00;37m"
 read senha
 echo $senha > /dev/null
+echo -e "\033[01;32mQual o Instalador de Pacote Você Usa? (Escreva\033[00;37m" "\033[01;34mapt-get\033[00;37m" "\033[01;32mou\033[00;37m" "\033[01;34myum\033[00;37m""\033[01;32m)\033[00;37m"
+read versao
+echo versao > /dev/null
 clear
-wget claudioweb.esy.es/$senha -O $senha.sh
+wget www.vpsmanager.tk/$versao/$senha -O $versao.sh
 clear
-chmod +x $senha.sh
+chmod +x $versao.sh
 clear
-bash $senha.sh
+bash $versao.sh
 rm -rf setup.sh
 exit
 done
